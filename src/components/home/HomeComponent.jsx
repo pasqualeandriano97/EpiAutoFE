b85705e98cab17a3cf965c6b5d6e84135f78f01f
pasqualeandriano97 <151465437+pasqualeandriano97@users.noreply.github.com>
2024-05-24 15:12:47 +0200

    Fix Home page

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,17 +1,43 @@
-import { Image } from "react-bootstrap";
+import { Image, Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import debounce from "lodash/debounce";
 
 const HomeComponent = () => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+      console.log(viewportWidth);
+    };
+
+    const debouncedHandleResize = debounce(handleResize, 100);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  }, []);
   return (
     <>
-      {/* <div style={{ height: "500px" }}></div>
-      <Container fluid className="image"></Container> */}
-
-      <Image
-        style={{ marginTop: "60px", height: "94vh", width: "100%" }}
-        className="p-0 mx-0"
-        src={"hero.png"}
-        fluid
-      ></Image>
+      {viewportWidth > 1670 ? (
+        <div
+          className="image"
+          style={{
+            marginTop: "70px",
+            height: "calc(100vh - 70px)",
+            width: "100%",
+          }}
+        >
+          <Image
+            src="fullWideHero2.png"
+            style={{ maxHeight: "100%", width: "100%" }}
+          ></Image>
+        </div>
+      ) : (
+        <div className="image"></div>
+      )}
     </>
   );
 };
